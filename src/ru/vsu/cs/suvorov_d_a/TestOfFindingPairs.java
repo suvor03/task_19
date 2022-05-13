1f7905155a4b7e63cfbccbb3bdbae80a1fd493f2
suvor03 <90615074+suvor03@users.noreply.github.com>
2022-05-13 13:01:02 +0300

    Add files via upload

--- src/ru/vsu/cs/suvorov_d_a/TestOfFindingPairs.java
+++ src/ru/vsu/cs/suvorov_d_a/TestOfFindingPairs.java
@@ -8,9 +8,27 @@ import ru.vsu.cs.suvorov_d_a.utils.ArrayUtils;
 public class TestOfFindingPairs {
     @Test
     public void testingArr1() {
-        int[] testingInputArray = ArrayUtils.readIntArrayFromFile("input.txt");
+        int[] testingInputArray = ArrayUtils.readIntArrayFromFile("testSrc/testCase/input1.txt");
         List<Integer> actualList = DifferentPairs.findPairsWithForLoop(testingInputArray, 5);
-        int[] correctOutputList = ArrayUtils.readIntArrayFromFile("testSrc/testResult/output1.txt");
+        List<Integer> correctOutputList = ArrayUtils.readListFromFile("testSrc/testResult/output1.txt");
+
+        Assert.assertEquals(correctOutputList, actualList);
+    }
+
+    @Test
+    public void testingArr2() {
+        int[] testingInputArray = ArrayUtils.readIntArrayFromFile("testSrc/testCase/input2.txt");
+        List<Integer> actualList = DifferentPairs.findPairsWithForLoop(testingInputArray, -3);
+        List<Integer> correctOutputList = ArrayUtils.readListFromFile("testSrc/testResult/output2.txt");
+
+        Assert.assertEquals(correctOutputList, actualList);
+    }
+
+    @Test
+    public void testingArr3() {
+        int[] testingInputArray = ArrayUtils.readIntArrayFromFile("testSrc/testCase/input3.txt");
+        List<Integer> actualList = DifferentPairs.findPairsWithForLoop(testingInputArray, 6);
+        List<Integer> correctOutputList = ArrayUtils.readListFromFile("testSrc/testResult/output3.txt");
 
         Assert.assertEquals(correctOutputList, actualList);
     }
